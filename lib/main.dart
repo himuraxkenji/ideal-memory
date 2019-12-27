8c30a42ccec203830c9bb37c906b0357de81b18d
himuraxkenji <aaron.ariperto@gmail.com>
2019-12-26 23:41:18 -0300

    List view component

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// Mis paquetes
+import 'package:componentes/src/pages/home_temp.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      home:HomePageTemp()
+    );
+  }
+}
