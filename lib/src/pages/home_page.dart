0b92d0ab64a11304fca814be66c8cbb654aca99f
himuraxkenji <aaron.ariperto@gmail.com>
2019-12-27 03:45:35 -0300

    Get icon of string

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
 
@@ -28,13 +29,13 @@ class HomePage extends StatelessWidget {
   }
 
   List<Widget> _listaItems(List<dynamic> data) {
-    
+
     final List<Widget> opciones = [];
 
     data.forEach((opt){
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
-        leading: Icon(Icons.account_circle, color: Colors.blue),
+        leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: (){}
       );
