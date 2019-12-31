8c357d2a1f1655ade95fb532c6d4515e3baecdc7
himuraxkenji <aaron.ariperto@gmail.com>
2019-12-31 00:42:28 -0300

    Confurations of idioms

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // import 'package:componentes/src/pages/home_temp.dart';
 import 'package:componentes/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('es', 'ES'),
+      ],
       // home:HomePage()
       initialRoute: '/',
       routes: getApplicationRoutes(),
